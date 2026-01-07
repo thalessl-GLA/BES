6eb4402e2606f7f0bf568d43a86b6dc42083e838
thalessl-GLA <t.souza-lima.1@research.gla.ac.uk>
2026-01-07 09:37:06 -0300

    Create BES_Macro_panel_creation.R

---
+++ BES_Macro_panel_creation.R
@@ -0,0 +1,20 @@
+# claimant count
+#https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment/datasets/claimantcountandvacanciesdataset
+#Claimant Count : K02000001 UK : People : SA : Thousands
+#BCJD
+
+# Unmeployment rate 
+#https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms
+
+# CPI
+#https://www.ons.gov.uk/economy/inflationandpriceindices/datasets/consumerpriceindices
+
+#CPIH ANNUAL RATE 00: ALL ITEMS 2015=100
+#L55O
+
+# GDP per capita level
+
+#https://www.ons.gov.uk/economy/grossdomesticproductgdp/timeseries/ihxw/
+  
+  
+  
